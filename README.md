7d28d373bb8c85fa9c34e4c6eed911524123caa2
tsuyo-244 <84175792+tsuyo-244@users.noreply.github.com>
2021-06-04 10:22:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# テーブル設計
+
+## users テーブル
+
+| Column     | Type   | Options     |
+| --------   | ------ | ----------- |
+| name       | string | null: false |
+| email      | string | null: false |
+| password   | string | null: false |
+| profile    | text   | null: false |
+| occupation | text   | null: false |
+| position   | text   | null: false |
+
+### Association
+
+- has_many :prototype
+- has_many :comments
+
+## prototype テーブル
+
+| Column     | Type       | Options     |
+| ---------- | ---------- | ----------- |
+| title      | string     | null: false |
+| catch_copy | text       | null: false |
+| concept    | text       | null: false |
+| user       | references | null: false |
+### Association
+
+- has_many  :comments
+- belong_to : user
+
+## comments テーブル
+
+| Column    | Type       | Options                        |
+| --------- | ---------- | ------------------------------ |
+| user      | references | null: false, foreign_key: true |
+| prototype | references | null: false, foreign_key: true |
+| text      | text       | null: false, foreign_key: true |
+
+### Association
+
---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,57 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+    @prototypes = Prototype.all#includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+      #pathを指定しなくても:newだけで新規作成ページに
+      #移行(今回はそのままのページに止まる)できる
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+                                      #↑必要な全てのカラムを書き込まなければ投稿が保存されない
+  end
+end
